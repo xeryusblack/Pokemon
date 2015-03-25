da81655e12b61cb4c8a40a6557fc8bc18e594e37
Wilbert <wilbertxeryus@gmail.com>
2015-03-25 20:29:12 +0800

    wilbert

---
+++ PokemonClient/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PokemonClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client client = new
+            ServiceReference1.Service1Client();
+            string returnString;
+
+            returnString = client.GetData(textBox1.Text);
+            label1.Text = returnString;
+        }
+
+
+    }
+}
